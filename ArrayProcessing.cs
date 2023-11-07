4c9b8d1dffdba5163418008fbd47986d767923cb
Алексей Н. Вовк <van@ets.nnov.ru>
2023-11-07 22:49:20 +0300

    Внесены исправления в  текст программы. Программа отлажена

--- ArrayProcessing.cs
+++ ArrayProcessing.cs
@@ -10,10 +10,12 @@
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []
 */
-
-string[] baseArray = {"Hello", "2", "world", ":-)"};
-string[] newStrArr = new string[0];
-string tmp = string.Empty;
+string[] baseArray = {"Hello", "2", "world", ":-)"};        // Проверяемый масив
+// string[] baseArray = {"1234", "1567", "-2", "computer science"};        // Проверяемый масив
+// string[] baseArray = {"Russia", "Denmark", "Kazan"};        // Проверяемый масив
+string[] newStrArr = new string[0];                         // Пустой массив для наполнения
+string tmp = string.Empty;                                  // Пустая строка  
+int k = 0;                                                  // Счетчик выполнений условия
 
 for (int i = 0; i < baseArray.Length; i++)
 {
@@ -21,11 +23,13 @@ for (int i = 0; i < baseArray.Length; i++)
     if (tmp.Length <= 3)
     {
         Array.Resize(ref newStrArr, newStrArr.Length + 1);
-        newStrArr[i] = tmp;
+        newStrArr[k] = tmp;
+        k++;
     }
 }
 
 if (newStrArr.Length > 0)
 {
-    for (int i = 0; i < newStrArr.Length; i++) Console.WriteLine(newStrArr[i]);
+    for (int i = 0; i < newStrArr.Length; i++) Console.Write($"\"{newStrArr[i]}\" ");
 }
+else Console.WriteLine("В исходном массиве нет строк длинной = 3 или менее символам");
